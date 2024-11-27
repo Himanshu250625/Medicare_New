3bb4be02e90ba7f9d79f1e6664626eb3a7298715
himanshusingh9793 <him979322@gmail.com>
2024-11-27 22:55:30 +0530

    Update Header.jsx
    minor css fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,10 +14,10 @@ const Header = () => {
     });
   }, []);
   return (
-    <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20 ">
+    <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
       {/* --------- Header Left --------- */}
       <div
-        className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]"
+        className="w-1/2 lg:w-1/3 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]"
         data-aos="fade-right"
       >
         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
@@ -46,9 +46,9 @@ const Header = () => {
           className="flex justify-center items-center w-full"
           data-aos="zoom-in"
         >
-          <img className="w-[380px] " src={heroImg01} alt="" />
+          <img className="w-[350px] " src={heroImg01} alt="" />
         </div>
-        <div className="mt-[80px]">
+        <div className="mt-[80px] h-[480px] flex flex-col justify-evenly">
           <img
             className="w-full mb-[30px]"
             src={heroImg02}
